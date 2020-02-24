5e64623ba9e41e515b99ac02502e55e937fa22f2
Ghani Rafif <ghani.rafif.2112@gmail.com>
2020-02-24 12:20:10 +0700

    Add deploy.sh

--- Amiyaa/xfwm4/deploy.sh
+++ Amiyaa/xfwm4/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 shopt -s extglob
 
+cd "$(dirname $0)"
 for filename in $(ls !(title).xpm) 
 do
   ln -s $(pwd)/$filename $(pwd)/${filename%.*}-active.xpm
@@ -14,10 +15,3 @@ do
   ln -s $(pwd)/title.xpm $(pwd)/title-$suffix-active.xpm
   ln -s $(pwd)/title.xpm $(pwd)/title-$suffix-inactive.xpm
 done
-
-# for prefix in top-left top-right
-# do
-#   ln -s $(pwd)/title.xpm $(pwd)/$prefix-active.xpm
-#   ln -s $(pwd)/title.xpm $(pwd)/$prefix-inactive.xpm
-# done
-  
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+case $1 in
+  [Aa]miya)
+    themesname=Amiyaa
+esac
+
+# Check if the theme directory already exist
+if [[ -d "$HOME/.themes/$themesname" ]];then
+  rm -r "$HOME/.themes/$themesname" && mkdir -p "$HOME/.themes/$themesname"
+fi
+
+# Copy the theme to ~/.themes
+cp -r "$(dirname $0)/$themesname" ~/.themes/
+"$HOME/.themes/$themesname/deploy.sh"
