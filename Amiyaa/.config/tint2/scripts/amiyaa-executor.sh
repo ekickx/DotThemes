55d160bf9016c64420972e1322caac092d994d2d
Ghani Rafif <ghani.rafif.2112@gmail.com>
2020-08-31 18:52:37 +0700

    Amiyaa => Tint2: Fix amiyaa-executor.sh when detecting workspace

--- Amiyaa/.config/tint2/scripts/amiyaa-executor.sh
+++ Amiyaa/.config/tint2/scripts/amiyaa-executor.sh
@@ -8,7 +8,7 @@ previous_oppacity="$(cat ~/.config/tint2/tint2rc | sed -n 's/^.*\(#2b2b2b \)\(.*
 echo "/home/ekickx/.config/tint2/icons/amiyaa-menu.svg"
 
 cat ~/.config/tint2/tint2rc | sed -i 's/\(#2b2b2b\)\(.*\)/\1 0/' ~/.config/tint2/tint2rc
-for i in $(wmctrl -l | grep -E "(^|\s)"$current_workspace"($|\s)" | cut -d " " -f 1); do
+for i in $(wmctrl -l | cut -d " " -f -3 | grep -E "(^|\s)"$current_workspace"($|\s)" | cut -d " " -f 1); do
   exist="$(xprop -id "$i" | grep "_NET_WM_STATE(ATOM)" | grep "MAXIMIZED_VERT" | grep "MAXIMIZED_HORZ" | grep -v "HIDDEN")"
   if [[ -n "$exist" ]]; then
     cat ~/.config/tint2/tint2rc | sed -i 's/\(#2b2b2b\)\(.*\)/\1 100/' ~/.config/tint2/tint2rc
